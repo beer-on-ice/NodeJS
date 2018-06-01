6aeb5a741b49efc5bba23bede932debf00f842c2
孤独的箭 <li997477295@outlook.com>
2018-06-01 17:47:15 +0800

    access_token获取
    问题： wechat.txt已存在access_token时无法继续下一步

---
+++ wechat-platform/app.js
@@ -0,0 +1,30 @@
+const Koa = require('koa')
+let path = require('path')
+
+let wechat = require('./wechat/P')
+let util = require('./libs/util')
+
+let wechat_file = path.join(__dirname,'./config/wechat.txt')
+
+let config = {
+    wechat: {
+        appId: 'wx65c6e5af07a88786',
+        appSecret: '01a5c36f2bec30dcedeead6fb0bc9ad5',
+        token: 'lijinchaojidatiancaihaha',
+        getAccessToken: function() {
+            return util.readFileAsync(wechat_file)
+        },
+        saveAccessToken: function() {
+            data = JSON.stringify(data)
+            return util.writeFileAsync(wechat_file,data)
+        }
+    }
+}
+
+let app = new Koa()
+
+app.use(wechat(config.wechat))
+
+app.listen(3000,function() {
+    console.log('Listening： 3000');
+})
