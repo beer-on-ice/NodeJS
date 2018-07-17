7f8e6303fad3ce618073e334ee67bd5a00941cec
beer-on-ice <li997477295@outlook.com>
2018-07-17 14:51:32 +0800

    koa-session

--- node_KOA_FORESHOW/koa_practise/server/index.js
+++ node_KOA_FORESHOW/koa_practise/server/index.js
@@ -1,5 +1,7 @@
 const Koa = require('koa')
 const logger = require('koa-logger')
+const compose = require('koa-compose')
+const session = require('koa-session')
 const app = new Koa()
 const mid1 = async (ctx, next) => {
   ctx.body = 'Hi, '
@@ -18,16 +20,31 @@ const mid3 = async (ctx, next) => {
   ctx.body = ctx.body + ' 4'
 }
 
-function tail (i) {
-  if (i > 3) return i
-  console.log('修改前 ', i)
-  return tail(i + 1)
-}
-tail(0)
+const mid = compose([mid1, mid2, mid3])
+
+// function tail (i) {
+//   if (i > 3) return i
+//   console.log('修改前 ', i)
+//   return tail(i + 1)
+// }
+// tail(0)
 
 app.use(logger())
-app.use(mid1)
-app.use(mid2)
-app.use(mid3)
+
+app.use(mid)
+// app.use(mid1)
+// app.use(mid2)
+// app.use(mid3)
+
+app.keys = ['Hi LUKE']
+app.use(session(app))
+
+app.use((ctx, next) => {
+  if (ctx.path === '/favicon.ico') return
+  let n = ctx.session.views || 0
+  ctx.session.views = ++n
+  ctx.body = ctx.body + '  ----' + n + ' views -----  '
+  next()
+})
 
 app.listen(8088)
