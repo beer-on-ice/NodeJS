50dd707b50a2840596d0cffe4ade4eee90208a5b
beer-on-ice <li997477295@outlook.com>
2018-07-18 13:53:49 +0800

    puppeteer获取豆瓣电影信息

--- node_KOA_FORESHOW/server/crawler/trailer-list.js
+++ node_KOA_FORESHOW/server/crawler/trailer-list.js
@@ -1,5 +1,5 @@
 const puppeteer = require('puppeteer')
-const url = 'https://movie.douban.com/explore#!type=movie&tag=%E7%83%AD%E9%97%A8&sort=recommend&page_limit=20&page_start=20'
+const url = 'https://movie.douban.com/tag/#/?sort=R&range=6,10&tags='
 const sleep = time => new Promise(resolve => [
   setTimeout(resolve, time)
 ])
@@ -8,19 +8,40 @@ const sleep = time => new Promise(resolve => [
   console.log('Start visit the target page')
   const browser = await puppeteer.launch({
     args: ['--no-sandbox'],
-    dumpio: false
+    dumpio: false,
+    headless: false
   })
   const page = await browser.newPage()
   await page.goto(url, {
-    waitUntil: 'networkidle'
+    waitUntil: 'networkidle2'
   })
   await sleep(3000)
-  await page.awaitForSelector('.more')
+  await page.waitForSelector('.more')
   for (let i = 0; i < 1; i++) {
     await sleep(3000)
     await page.click('.more')
   }
   const result = await page.evaluate(() => {
-
+    let $ = window.$
+    let items = $('.list-wp a')
+    let links = []
+    if (items.length >= 1) {
+      items.each((index, item) => {
+        let it = $(item)
+        let doubanId = it.find('div').data('id')
+        let title = it.find('.title').text()
+        let rate = Number(it.find('.rate').text())
+        let poster = it.find('img').attr('src').replace('s_ratio', 'l_ratio')
+        links.push({
+          doubanId,
+          title,
+          rate,
+          poster
+        })
+      })
+    }
+    return links
   })
+  browser.close()
+  console.log(result)
 })()
