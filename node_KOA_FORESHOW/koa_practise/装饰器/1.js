2e842a9a116ddd2ded1f7296f0030df4ad4440fa
beer-on-ice <li997477295@outlook.com>
2018-07-24 16:11:04 +0800

    装饰器实例

--- node_KOA_FORESHOW/koa_practise/装饰器/1.js
+++ node_KOA_FORESHOW/koa_practise/装饰器/1.js
@@ -1,9 +1,3 @@
-// 让bark变为只读属性
-function readonly (target, key, descriptor) {
-  descriptor.writeable = false
-  return descriptor
-}
-
 class Dog {
   constructor (name, voice) {
     this.name = name
@@ -15,5 +9,16 @@ class Dog {
   }
 }
 
+// 让bark变为只读属性
+function readonly (target, key, descriptor) {
+  console.log(target);
+  
+  descriptor.writeable = false
+  return descriptor
+}
+
 let pig = new Dog('xiaohei', 'wangwang')
+
+pig.bark = '123'
+
 console.log(pig.bark())
