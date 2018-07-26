26b7b251e7ad1ee42fd7ebd4c93b0d011f0f5b12
beer-on-ice <li997477295@outlook.com>
2018-07-26 10:33:54 +0800

    lodash实例

--- node_KOA_FORESHOW/koa_practise/lodash/1.js
+++ node_KOA_FORESHOW/koa_practise/lodash/1.js
@@ -1,3 +1,11 @@
 const _ = require('lodash')
+let arr
+let arr1 = [1, 2, 3, 4, 5]
 
-let arr = [1, 2, 3, 4, 5]
+// 拆分数组，第二个参数是每个数组区块长度
+arr = _.chunk(arr1, 2)
+console.log(arr)
+
+let arr2 = [1, false, '', 3]
+arr = _.compact(arr2)
+console.log(arr)
