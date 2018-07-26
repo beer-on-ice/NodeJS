625b6f9b1c36d70f97d73ed70929a69e15583256
beer-on-ice <li997477295@outlook.com>
2018-07-26 15:25:47 +0800

    暴露路由层面api

--- node_KOA_FORESHOW/server/lib/decorator.js
+++ node_KOA_FORESHOW/server/lib/decorator.js
@@ -1,14 +1,12 @@
-import { isArray } from 'util'
-
 const Router = require('koa-router')
 const {resolve} = require('path') // node模块
 const glob = require('glob') // node模块
-const _ = require('lodash') 
+const _ = require('lodash')
 
 const symbolPrefix = Symbol('prefix') // 原始数据类型Symbol，表示独一无二的值,可以接受一个字符串作为参数
 const routerMap = new Map()
 
-const isArray = c => _.isArray(c)?c:[c]
+const isArray = c => _.isArray(c) ? c : [c]
 
 export class Route {
   constructor (app, apiPath) {
@@ -21,16 +19,16 @@ export class Route {
     for (let [conf, controller] of routerMap) {
       const controllers = isArray(controller)
       const prefixPath = conf.target[symbolPrefix]
-      if(prefixPath) prefixPath = normalizePath(prefixPath)
+      if (prefixPath) prefixPath = normalizePath(prefixPath)
       const routerPath = prefixPath + conf.path
-      this.router[conf.method](routerPath,...controllers)
+      this.router[conf.method](routerPath, ...controllers)
     }
     this.app.use(this.router.routes())
     this.app.use(this.router.allowedMethods())
   }
 }
 
-export const normalizePath = path => path.startWith('/') ? path : `/${path}`
+export const normalizePath = path => path.startsWith('/') ? path : `/${path}`
 
 export const router = conf => (target, key, decriptor) => {
   conf.path = normalizePath(conf.path)
--- node_KOA_FORESHOW/server/routes/movie.js
+++ node_KOA_FORESHOW/server/routes/movie.js
@@ -1,16 +1,24 @@
 const mongoose = require('mongoose')
 const Router = require('koa-router')
 const router = new Router()
-const {controller,get,post,put} = require('../lib/decorator')
+const {
+  controller,
+  get,
+  post,
+  put
+} = require('../lib/decorator')
+const {
+  getAllMovies,
+  getMovieDetail,
+  getRelativeMovies
+} = require('../service/movie')
 
 @controller('/api/v0/movies')
 export class movieController {
   @get('/')
   async getMovies (ctx,next) {
-    const Movie = mongoose.model('Movie')
-    const movies = await Movie.find({}).sort({
-      'meta.createdAt': -1
-    })
+    const {type,year} = ctx.query
+    const movies = await getAllMovies(type,year)
     ctx.body = {
       movies
     }
@@ -18,11 +26,16 @@ export class movieController {
 
   @get('/:id')
   async getMovieDetail (ctx, next) {
-    const Movie = mongoose.model('Movie')
     const id = ctx.params.id
-    const movie = await Movie.findOne({_id: id})
+    const movie = await this.getMovieDetail(id)
+    const relativeMovies = await getRelativeMovies(movie)
+
     ctx.body = {
-      movie
+      data: {
+        movie,
+        relativeMovies
+      },
+      success: true
     }
   }
 }
